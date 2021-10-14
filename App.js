dc2ae86470e0dd1d6942c15495f303796827eb08
Silvesterrr <sylwester.jarosz@spoko.pl>
2021-10-15 00:14:03 +0200

    Post props

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 import HomeScreen from './src/screens/home';
 import Post from './src/components/Post';
 
+import feed from './assets/data/feed';
 
+const post1 = feed[0];
+const post2 = feed[1];
+const post3 = feed[2];
 export default function App() {
   return (
     <>
     <StatusBar barStyle="dark-content"/>
-    <SafeAreaView>
-      <Post/>
-    </SafeAreaView>
+    <ScrollView>
+      <Post post={post1} />
+      <Post post={post2} />
+      <Post post={post3} />
+    </ScrollView>
     </>
   );
 }

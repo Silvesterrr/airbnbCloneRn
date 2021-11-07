dfe751abb0bda3fade0be8718610570ea5d5b063
Silvesterrr <sylwester.jarosz@spoko.pl>
2021-11-07 22:34:19 +0100

    map sync done

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { View, Text, FlatList, useWindowDimensions } from 'react-native'
 import MapView, {Marker} from 'react-native-maps'
 
@@ -14,11 +14,45 @@ const SearchResultsMap = () => {
     const [selectedPlaceId,setSelectedPlaceId] = useState(null);
 
     const width = useWindowDimensions().width;
+
+    const flatlist = useRef();
+
+    const map = useRef();
+
+    const viewConf = useRef({itemVisiblePercentThreshold: 100});
+    const onViewChanged = useRef(({viewableItems}) => {
+        if (viewableItems.length>0){
+            const selectedPlace = viewableItems[0].item;
+            setSelectedPlaceId(selectedPlace.id);
+        }});
+
+        
+    const Cont = (index) => {
+        flatlist.current.scrollToIndex({index}); 
+
+        const selectedPlace = places[index];
+        const region = {
+            latitude: selectedPlace.coordinate.latitude,
+            longitude: selectedPlace.coordinate.longitude,
+            latitudeDelta: 0.8,
+            longitudeDelta: 0.8
+        }
+        map.current.animateToRegion(region);
+    }
+    useEffect(() => {
+        if (!selectedPlaceId || !flatlist){
+            return
+        }
+        const index = places.findIndex(place => place.id === selectedPlaceId)
+        setTimeout(() => Cont(index), 300);
+        
+    }, [selectedPlaceId])
     return (
         <View style={styles.container}>
             <MapView 
+                ref={map}
                 style={styles.map} 
-                initialRegion={{
+                initialRegio ={{
                     latitude: 28.3279822,
                     longitude: -16.5124847,
                     latitudeDelta: 0.8,
@@ -37,6 +71,7 @@ const SearchResultsMap = () => {
             </MapView>
             <View style={{position: 'absolute', bottom: 10, left:0}}>
                 <FlatList 
+                    ref={flatlist}
                     data={places}
                     renderItem={({item}) => <PostCarouselItem post={item} />}
                     horizontal
@@ -45,6 +80,9 @@ const SearchResultsMap = () => {
                     snapToInterval={width-60}
                     snapToAlignment={'center'}
                     decelerationRate={'fast'}
+                    overScrollMode={'never'}
+                    viewabilityConfig={viewConf.current}
+                    onViewableItemsChanged={onViewChanged.current}
                 />
 
             </View>
